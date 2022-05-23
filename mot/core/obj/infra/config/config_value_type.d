0c307fc7c1334adc6abc50ebdffd2f057184c78b
Codebells <1347103071@qq.com>
2022-05-23 15:48:02 +0800

    2022.5.23

--- mot/core/obj/infra/config/config_value_type.d
+++
@@ -1,4 +0,0 @@
-../obj/infra/config/config_value_type.o: \
- infra/config/config_value_type.cpp infra/config/config_value_type.h
-
-infra/config/config_value_type.h:
--- mot/core/obj/infra/synchronization/rw_lock.d
+++
@@ -1,4 +0,0 @@
-../obj/infra/synchronization/rw_lock.o: infra/synchronization/rw_lock.cpp \
- infra/synchronization/rw_lock.h
-
-infra/synchronization/rw_lock.h:
